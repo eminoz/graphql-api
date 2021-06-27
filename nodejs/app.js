5d1c690a4c8d025d3bbe54efd31906b37b86223a
eminoz <eminoz.0559@gmail.com>
2021-06-27 18:28:25 +0300

    graphql-api

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -34,8 +33,8 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+
+app.use(bodyParser.json());
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
@@ -99,7 +98,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/messages?retryWrites=true'
+    'your mongodb address'
   )
   .then(result => {
     app.listen(8080);
